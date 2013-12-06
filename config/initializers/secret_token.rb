a2485e8ccf3bf11a2dd714bb33d3fe3e04deb7d6
FUKUI Osamu <ofukui@irubysystems.com>
2013-12-06 10:12:24 +0900

    project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ilovemogok::Application.config.secret_key_base = 'f0f878116d2fcdb7205ff81022f87315e2396421ba4a7e1df07c37cc5dbdc2630b74ce192d20f76ec00dcbb8f3197babf120e7b33ee25c169165455afee240d2'
